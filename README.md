0251a6e8be33767607a2f8009357a0d2592651d4
vagrant <vagrant@vagrant.vm>
2018-01-27 16:54:37 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# SimpleMovieDB
-Simple Movie Application with Python and sqlite with CRUD funcionality
+# Movie and Actors
+
+A simple app about famous movies and actors.
+
+This application developed for the purpose of study od following technologies:
+
+- Python Flask
+- Data base (sqlite and sqlalchemy)
+- CRUD funtionality using Flask template
+- Git version control
---
+++ my_project.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template 
+app = Flask(__name__) ### Instance of the Flask with name of the running application as an argument
+
+#################################################################################################
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Movie, Actor
+
+engine = create_engine('sqlite:///movieactors.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+#################################################################################################
+@app.route('/')
+@app.route('/movies/<int:movie_id>/')
+def movieActors(movie_id):
+	movie = session.query(Movie).filter_by(id = movie_id).one()
+	actors = session.query(Actor).filter_by(movie_id = movie.id)
+
+	# output = ''
+	# for i in actors:
+	# 	output += i.name
+	# 	output += '</br>'
+	# 	output += str(i.age) 
+	# 	output += '</br>'
+	# 	output += i.biography
+	# 	output += '</br></br>'
+	return render_template('menu.html', movie = movie, actors = actors)
+
+# Task 1: Create route for newActor function here
+@app.route('/movie/<int:movie_id>/new/')
+def newActor(movie_id):
+	return "Page to create new Actor. Task 1 complete"
+
+# Task 2: Create route for editActor function here
+@app.route('/movie/<int:movie_id>/<int:actor_id>/edit/')
+def editActor(movie_id, actor_id):
+	return "Page to edit new Actor. Task 2 complete"
+
+# Task 3: Create route for deleteActor function here
+@app.route('/movie/<int:movie_id>/<int:actor_id>/delete/')
+def deleteActor(movie_id, actor_id):
+	return "Page to delete new Actor. Task 3 complete"
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run(host = '0.0.0.0', port = 5000)
